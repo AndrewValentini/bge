18ee6eafa4cfd5180a071640d8174406e8de1ac4
Andrew Valentini <95447370+AndrewValentini@users.noreply.github.com>
2022-07-07 11:44:06 -0500

    Update randomquote.py

--- randomquote.py
+++ randomquote.py
@@ -1,4 +1,11 @@
 import csv
 import random
 
-def get_random_quote(quotes_file = 'bge.csv')
+def get_random_quote(quotes_file = 'bge.tsv'): 
+  with open(quotes_file) as tsvfile: 
+    quotes = [{"quote:" = line[5]} for line in csv.reader(tsvfile)}]
+  return random.choice(quotes)
+              
+              
+quote = get_random_quote()
+print(quote)
