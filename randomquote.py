3d6ca6f4c92c452e7ade9fed0f33d336879d9f02
Andrew Valentini <95447370+AndrewValentini@users.noreply.github.com>
2022-07-07 11:48:19 -0500

    Update randomquote.py

--- randomquote.py
+++ randomquote.py
@@ -5,7 +5,8 @@ def get_random_quote(quotes_file = 'bge.tsv'):
   with open(quotes_file) as tsvfile: 
     quotes = [{"quote:" = line[5]} for line in csv.reader(tsvfile)}]
   return random.choice(quotes)
-              
-              
-quote = get_random_quote()
-print(quote)
+
+
+if __name__ == 'main': 
+  quote = get_random_quote()
+  print(quote)
